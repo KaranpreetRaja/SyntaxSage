76671eda9a6bbfd12cfcf4fecff8a0cf07ede794
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-02-07 23:48:29 -0500

    Create PageNavigator.java

---
+++ src/GUI/PageNavigator.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PageNavigator extends JPanel{
+    public PageNavigator(){
+        super(new GridLayout(1, 1));
+        JTabbedPane tabbedPlane = new JTabbedPane();
+
+    }
+}
+
