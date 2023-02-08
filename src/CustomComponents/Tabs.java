2dd4cbdd0822253339adf1f27f3a44ca4b1d18e0
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-02-08 13:44:22 -0500

    Creates Tabs.java

---
+++ src/CustomComponents/Tabs.java
@@ -0,0 +1,16 @@
+
+public class Tabs{
+    public String name;
+    public ImageIcon icon;
+    public JComponent pannel;]
+    public String discription;
+
+
+    public Tabs(String name, ImageIcon icon, JComponent pannel, String discription){
+        this.name = name;
+        this.icon = icon;
+        this.pannel = pannel;
+        this.discription = discription;
+    }
+
+}
