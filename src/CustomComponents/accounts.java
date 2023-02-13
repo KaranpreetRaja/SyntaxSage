4e707b12785ace8028974a5174ceb0e5c722d596
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-02-13 11:26:50 -0500

    Update accounts.java

--- src/CustomComponents/accounts.java
+++ src/CustomComponents/accounts.java
@@ -1,33 +1,31 @@
-
-import okhttp3.Request;
-import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 import java.util.ArrayList;
 
-class Account{
-    
+class Account {
+
     private String accountName;
     private String accountPass;
-    private String UUID;
+    private int ID;
     private ArrayList<String> classes;
+    public static int curID = 0;
 
     //
     public Account() {
         this.accountName = "";
         this.accountPass = "";
-        this.UUID = "";
+        this.ID = curID;
+        curID++;
         this.classes = new ArrayList<String>();
     }
 
     public Account(String accountName, String accountPass) {
         this.accountName = accountName;
         this.accountPass = accountPass;
-        this.UUID = generateUUID();
+        this.ID = curID;
+        curID++;
         this.classes = new ArrayList<String>();
     }
 
-    public static Account createAccount(String accountName, String accountPass){
+    public static Account createAccount(String accountName, String accountPass) {
         Account newAccount = new Account(accountName, accountPass);
         return newAccount;
     }
@@ -36,15 +34,15 @@ class Account{
         return this.accountName;
     }
 
-    public int getUUID() {
-        return this.UUID;
+    public int ID() {
+        return this.ID;
     }
 
-    private void setUsername(String newUsername){
+    private void setUsername(String newUsername) {
         this.accountName = newUsername;
     }
 
-    private void setPassword(String newPassword){
+    private void setPassword(String newPassword) {
         this.accountPass = newPassword;
     }
 
@@ -53,24 +51,14 @@ class Account{
     }
 
     private void enrollClasses(String newClass) {
-        //Check if Language exists in database first
+        // Check if Language exists in database first
         this.classes.add(newClass);
     }
 
     private void unenrollClasses(String oldClass) {
-        if (this.class.contains(oldClass)) {
+        if (this.classes.contains(oldClass)) {
             this.classes.remove(oldClass);
         }
     }
-
-    public String generateUUID() throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url("https://www.uuidtools.com/api/generate/v5/namespace/ns:url/name/https://www.uuidtools.com/generate").build();
-        try{
-            Response response = client.newCall(request).execute()
-            return response.body().string();
-        }
-    }
-
 
 }
