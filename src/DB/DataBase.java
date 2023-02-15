02b38f83994de45b2242346b51b47775aa62a1e9
Rajendra1308 <rajendra1308.rb.rb@gmail.com>
2023-02-14 19:01:02 -0500

    method addData()

---
+++ src/DB/DataBase.java
@@ -0,0 +1,66 @@
+package DB;
+
+import java.sql.*;
+
+/*
+Update information
+Add information
+Delete Information
+get information based upon given input usually id
+
+NOTE: There must be a server hosted locally with a Database named syntaxsage. A table with
+
+ */
+public class DataBase {
+    String username;
+    String password;
+    String classes;
+    String experience;
+    String accountCreateDate;
+
+
+    //Constructor
+
+
+    public DataBase( String username, String password, String classes, String experience, String accountCreateDate) {
+        this.username = username;
+        this.password = password;
+        this.classes = classes;
+        this.experience = experience;
+        this.accountCreateDate = accountCreateDate;
+    }
+    //connect to database
+    public void connectToDataBase(){
+        String url="jdbc:mysql://localhost:3306/project";
+        String uname="rajendra";
+        String passwordForDB="rajendra";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Found.");
+            Connection connection = DriverManager.getConnection(url, uname, passwordForDB);
+
+
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void addData()  {
+        try {
+            String url = "jdbc:mysql://localhost:3306/project";
+            String uname = "rajendra";
+            String passwordForDB = "rajendra";
+            Connection connection = DriverManager.getConnection(url, uname, passwordForDB);
+            Statement statement = connection.createStatement();
+           String query= String.format("insert into  syntaxsage (username,password,classes,experience,accountCreateDate) values ('%s','%s','%s','%s','%s')",this.username,this.password,this.classes,this.experience,this.accountCreateDate);
+            boolean result = statement.execute(query);
+        }
+        catch (SQLException exception){
+            System.out.println("SQL Exception in addData() method");
+        }
+
+    }
+}
