97ab4a604b5a0d6c1a6076ec79d97be9f04c295a
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-06 15:14:01 -0500

    Update Lesson.java
    Added Setters and Getters for Lesson.java

--- src/CustomComponents/Lesson.java
+++ src/CustomComponents/Lesson.java
@@ -1,13 +1,13 @@
 package CustomComponents;
 
 public class Lesson {
-	private int lessonID;
 	public static int curID = 0;
+
+	private int lessonID;
 	private String name;
 	public int noQuestions;
     public int curQuestion;
     public Question[] questions;
-	
 	
 	
 	public Lesson(String name, int noQuestions, Question[] questions) {
@@ -34,5 +34,53 @@ public class Lesson {
         }
         return null;
     }
+
+	public static int getCurID() {
+		return curID;
+	}
+
+	public static void setCurID(int curID) {
+		Lesson.curID = curID;
+	}
+
+	public int getLessonID() {
+		return lessonID;
+	}
+
+	public void setLessonID(int lessonID) {
+		this.lessonID = lessonID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNoQuestions() {
+		return noQuestions;
+	}
+
+	public void setNoQuestions(int noQuestions) {
+		this.noQuestions = noQuestions;
+	}
+
+	public int getCurQuestion() {
+		return curQuestion;
+	}
+
+	public void setCurQuestion(int curQuestion) {
+		this.curQuestion = curQuestion;
+	}
+
+	public Question[] getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(Question[] questions) {
+		this.questions = questions;
+	}
 
 }
