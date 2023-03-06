e29ac1e72ebdaf96cbe4c877574c7dfa55e9782c
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-06 15:15:49 -0500

    Changed Serialisation for Course.java from Course to curID

--- src/CustomComponents/Course.java
+++ src/CustomComponents/Course.java
@@ -1,38 +1,40 @@
 package CustomComponents;
 
 import java.util.ArrayList;
-
 import java.io.Serializable;
 import java.io.*;
 
 public class Course implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private int courseId = 0; // UUID for class
-	public static int curID = 0; //UUID for 
+	private int courseId; // UUID for class
+	private static int curID = 0; //UUID for Current ID
 	private String name; //course name
 	private ArrayList<Lesson> lessons;
 	private int noLessons; // number of lessons
 	//TODO: Add badges array
 	
-	//private String fileName = "course.ser"; //path to ser file
+	// Constructor for Course
+	public Course(String name, ArrayList<Lesson> lessons){
+		this.name = name;
+		this.noLessons = 0;
+		this.courseId = curID;
+		curID++;
+		this.lessons = lessons;
+	}
 	
-	
-	//constructor
 	public Course(String name){
 		this.name = name;
-		
-		
 		this.noLessons = 0;
 		this.courseId = curID;
 		curID++;
 	}
 
-	// Serialize the object
-	public void saveCourse(String fileName) {
+	// Save the curID to file
+	public static void saveCurID(String fileName) {
 		try {
 			FileOutputStream fileOut = new FileOutputStream(fileName);
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(this);
+			out.writeObject(curID);
 			out.close();
 			fileOut.close();
 			System.out.println("Serialized data is saved in " + fileName);
@@ -41,22 +43,25 @@ public class Course implements Serializable {
 		}
 	}
 
-	// Deserialize the object
-	public static Course loadCourse(String fileName) {
-		Course course = null;
+	// Load the curID from file
+	public static void loadCurID(String fileName) {
 		try {
 			FileInputStream fileIn = new FileInputStream(fileName);
 			ObjectInputStream in = new ObjectInputStream(fileIn);
-			course = (Course) in.readObject();
+			curID = (int) in.readObject();
 			in.close();
 			fileIn.close();
 			System.out.println("Deserialized data is loaded from " + fileName);
 		} catch(IOException i) {
 			i.printStackTrace();
 		} catch(ClassNotFoundException c) {
-			System.out.println("Course class not found");
+			System.out.println("Class not found");
 			c.printStackTrace();
 		}
-		return course;
+	}
+
+	// Getter for curID
+	public static int getCurID() {
+		return curID;
 	}
 }
