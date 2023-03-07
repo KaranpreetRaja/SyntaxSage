910302c7154618095a3f9789018332e24d068409
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-07 17:05:41 -0500

    Create Enrollment.java

---
+++ src/CustomComponents/Enrollment.java
@@ -0,0 +1,47 @@
+package CustomComponents;
+import java.util.ArrayList;
+
+public class Enrollment {
+	private Account account;
+	private Course course;
+	private ArrayList<int> lessonsCompleted;
+	private boolean completed;
+	
+	
+	public Enrollment(Account acc, Course course){
+		this.account = acc;
+		this.course = course;
+		this.lessonsCompleted = 0;
+		this.completed = false;
+	}
+
+
+	public Account getAccount() {
+		return account;
+	}
+
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+
+	public Course getCourse() {
+		return course;
+	}
+
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+}
