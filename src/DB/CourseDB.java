9d412c0b80d6f0472a6c4a3e81eedbd82171fbb3
Rajendra1308 <rajendra1308.rb.rb@gmail.com>
2023-03-08 14:51:53 -0500

    constructor for CourseDB

---
+++ src/DB/CourseDB.java
@@ -0,0 +1,20 @@
+package DB;
+
+public class CourseDB {
+    int courseId;
+    String courseName;
+    int noLessons;
+    public CourseDB(){
+
+    }
+
+    public CourseDB(int courseId, String courseName, int noLessons) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.noLessons = noLessons;
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
