fa2dd727f4f7683e1bcc31083e40465bbfba2f37
Rajendra1308 <rajendra1308.rb.rb@gmail.com>
2023-03-08 16:53:09 -0500

    connectDatabase() for AccountDB

--- src/DB/AccountDB.java
+++ src/DB/AccountDB.java
@@ -1,5 +1,9 @@
 package DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AccountDB {
     private int ID;
     private String username;
@@ -21,6 +25,22 @@ public class AccountDB {
         this.experience = experience;
         this.creationDate = creationDate;
     }
-    
+    public void connectToDataBase(){
+        String url="jdbc:mysql://localhost:3306/project";
+        String uname="rajendra";
+        String passwordForDB="rajendra";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Found.");
+            Connection connection = DriverManager.getConnection(url, uname, passwordForDB);
 
+
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+   
 }
