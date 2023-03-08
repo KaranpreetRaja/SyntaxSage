02f1c7cf8e686dab008d496980adb38e325f2ff0
Rajendra1308 <rajendra1308.rb.rb@gmail.com>
2023-03-08 16:15:43 -0500

    addData() for CourseDB

--- src/DB/CourseDB.java
+++ src/DB/CourseDB.java
@@ -1,4 +1,6 @@
 package DB;
+import java.sql.*;
+
 
 public class CourseDB {
     int courseId;
@@ -13,8 +15,39 @@ public class CourseDB {
         this.courseName = courseName;
         this.noLessons = noLessons;
     }
-    public static void main(String[] args) {
-        
+    public void connectToDataBase(){
+        String url="jdbc:mysql://localhost:3306/project";
+        String uname="rajendra";
+        String passwordForDB="rajendra";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Found.");
+            Connection connection = DriverManager.getConnection(url, uname, passwordForDB);
+
+
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+    public void addData()  {
+        try {
+            String url = "jdbc:mysql://localhost:3306/project";
+            String uname = "rajendra";
+            String passwordForDB = "rajendra";
+            Connection connection = DriverManager.getConnection(url, uname, passwordForDB);
+            Statement statement = connection.createStatement();
+            String query= String.format("insert into  courses values ('%d','%s','%d')",this.courseId,this.courseName,this.noLessons);
+            boolean result = statement.execute(query);
+        }
+        catch (SQLException exception){
+            System.out.println("SQL Exception in addData() method");
+        }
+
+    }
+
 
 }
