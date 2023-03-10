d89b44aa04cba6f450bd23889c159dd39d76628f
EarmuffSlime <rxlyn03@gmail.com>
2023-03-09 22:17:02 -0500

    Added Custom Exception File and Classes

---
+++ src/CustomComponents/CustomException.java
@@ -0,0 +1,27 @@
+package CustomComponents;
+
+import java.lang.Exception.*;
+
+public class CustomException extends Exception {
+    public CustomException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+class AccountNotFoundException extends CustomException {
+    public AccountNotFoundException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+class AccountSignUpException extends CustomException {
+    public AccountSignUpException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+class AccountUsernameException extends CustomException {
+    public AccountUsernameException(String errorMessage) {
+        super(errorMessage);
+    }
+}
