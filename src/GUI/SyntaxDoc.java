035de96607a4fcc2b460767ac55769a283bd5d58
mahfoozm <mohammadmahfoozpersonal@gmail.com>
2023-04-03 17:48:26 -0400

    fix 403 for docpage (parse raw markdown instead)

--- src/GUI/SyntaxDoc.java
+++ src/GUI/SyntaxDoc.java
@@ -1,19 +1,23 @@
 package GUI;
 import java.awt.*;
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import javax.swing.*;
+import org.commonmark.node.*;
+import org.commonmark.parser.*;
+import org.commonmark.renderer.html.*;
 
 public class SyntaxDoc extends JFrame {
     private static final long serialVersionUID = 1L;
     private static final String HOME_URL = "https://quickref.me/";
     private static final String[] LANGUAGES = {"HTML", "CSS", "CPP", "Python", "Java"};
     private static final String[] LANGUAGE_URLS = {
-        "https://quickref.me/html",
-        "https://quickref.me/css",
-        "https://quickref.me/cpp",
-        "https://quickref.me/python",
-        "https://quickref.me/java"
+        "https://raw.githubusercontent.com/Fechin/reference/main/source/_posts/html.md",
+        "https://raw.githubusercontent.com/Fechin/reference/main/source/_posts/css3.md",
+        "https://raw.githubusercontent.com/Fechin/reference/main/source/_posts/cpp.md",
+        "https://raw.githubusercontent.com/Fechin/reference/main/source/_posts/python.md",
+        "https://raw.githubusercontent.com/Fechin/reference/main/source/_posts/java.md"
     };
 
     private JEditorPane editorPane;
@@ -28,7 +32,8 @@ public class SyntaxDoc extends JFrame {
 
         editorPane = new JEditorPane();
         editorPane.setEditable(false);
-        editorPane.setContentType("text/html"); // Enable proper content rendering
+        // enable content rendering for markdown files
+        editorPane.setContentType("text/html");
         loadPage(language);
 
         JScrollPane scrollPane = new JScrollPane(editorPane);
@@ -60,15 +65,42 @@ public class SyntaxDoc extends JFrame {
         }
 
         try {
-            editorPane.setPage(new URL(url));
+            URL pageUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) pageUrl.openConnection();
+            connection.setRequestProperty("http.agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");// Add this line to set the user agent string
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                    content.append(System.lineSeparator());
+                }
+                in.close();
+
+                Parser parser = Parser.builder().build();
+                Node document = parser.parse(content.toString());
+
+                HtmlRenderer renderer = HtmlRenderer.builder().build();
+                String html = renderer.render(document);
+
+                editorPane.setText(html);
+            } else {
+                System.err.println("Error loading " + url + ": " + responseCode + " " + connection.getResponseMessage());
+                editorPane.setText("Error loading page: " + responseCode + " " + connection.getResponseMessage());
+            }
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error loading " + url + ": " + e);
-            editorPane.setText("Error loading page.");
+            editorPane.setText("Error loading page: " + e.getMessage());
         }
     }
 
+
+
     public static void main(String[] args) {
-        new SyntaxDoc("https://quickref.me/html"); // Pass the correct initial URL
+        new SyntaxDoc("https://raw.githubusercontent.com/Fechin/reference/main/source/_posts/html.md"); // Pass the correct initial URL
     }
 }
