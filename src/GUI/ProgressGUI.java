c7ae8cbe45e5b488c64b947a7f8a65cc56e25b07
mahfoozm <mohammadmahfoozpersonal@gmail.com>
2023-04-10 16:03:59 -0400

    fix #BR001 and code smells (#BR002)

--- src/GUI/ProgressGUI.java
+++ src/GUI/ProgressGUI.java
@@ -1,58 +1,73 @@
 package GUI;
+
 import javax.swing.*;
-
 import CustomComponents.Account;
-
 import java.awt.*;
 import java.awt.event.*;
 
 public class ProgressGUI extends JPanel {
     private static final long serialVersionUID = 6098546007197585733L;
-	private JProgressBar progressBar;
+    private JProgressBar progressBar;
     private JButton updateProgressButton;
     private Timer timer;
     private int progressValue = 0;
+    private static final int PROGRESS_MIN = 0;
+    private static final int PROGRESS_MAX = 100;
+    private Account acc; // Add acc as a field of ProgressGUI
 
     public ProgressGUI(Account acc) {
         super(new BorderLayout());
+        this.acc = acc; // Initialize acc field with parameter value
 
-        progressBar = new JProgressBar(0, 100);
-        progressBar.setValue(0);
+        // Initialize progress bar
+        progressBar = new JProgressBar(PROGRESS_MIN, PROGRESS_MAX);
+        progressBar.setValue(PROGRESS_MIN);
         progressBar.setStringPainted(true);
 
+        // Initialize update progress button
         updateProgressButton = new JButton("Update Progress Bar Value");
         updateProgressButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	if (acc.getExperience().equals("Master") && progressValue <=100) {	
-	                	if (e.getSource() == updateProgressButton) {
-	                		updateProgressButton.setEnabled(false);
-	                        timer.start();
-	                    } else if (e.getSource() == timer) {
-	                        progressValue++;
-	                        progressBar.setValue(progressValue);
-	                        progressBar.setString(progressValue + "%");
-	                    }
-                	}
-            	if (progressValue == 100) {
-                    timer.stop();
-                    updateProgressButton.setEnabled(true);
-                }
-                }}
-        		);
+                updateProgress();
+            }
+        });
 
+        // Create panel to hold progress bar and button
         JPanel panel = new JPanel(new BorderLayout());
-        
-        // Adding gap before the elements
+
+        // Add margin before the elements
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        
+
         panel.add(progressBar, BorderLayout.CENTER);
         panel.add(updateProgressButton, BorderLayout.PAGE_END);
 
         add(panel, BorderLayout.CENTER);
 
-        timer = new Timer(50, updateProgressButton.getActionListeners()[0]);
+        // Initialize timer for progress bar updates
+        timer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                progressValue++;
+                progressBar.setValue(progressValue);
+                progressBar.setString(progressValue + "%");
+                if (progressValue == PROGRESS_MAX) {
+                    timer.stop();
+                    updateProgressButton.setEnabled(true);
+                }
+            }
+        });
         timer.setInitialDelay(0);
     }
 
+    /**
+     * Updates progress bar value and starts timer to animate progress bar.
+     */
+    private void updateProgress() {
+        if (progressValue == PROGRESS_MAX || !acc.getExperience().equals("Master")) {
+            return;
+        }
+        updateProgressButton.setEnabled(false);
+        timer.start();
+    }
 }
