ef555562efca1c88f175971e280834d6fd758d7d
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-04-10 23:43:55 -0400

    Adds support for responding to specific questions

--- src/GUI/Forum.java
+++ src/GUI/Forum.java
@@ -7,8 +7,8 @@ import java.awt.event.ActionListener;
 
 public class Forum extends JPanel {
 
-    private JTextArea questionArea;
-    private JTextArea answerArea;
+    private DefaultListModel<String> discussionListModel;
+    private JList<String> discussionList;
     private JButton postQuestionButton;
     private JButton postAnswerButton;
 
@@ -20,14 +20,9 @@ public class Forum extends JPanel {
     }
 
     private void initComponents() {
-        questionArea = new JTextArea(10, 50);
-        questionArea.setEditable(false);
-        questionArea.setLineWrap(true);
-        questionArea.setWrapStyleWord(true);
-
-        answerArea = new JTextArea(10, 50);
-        answerArea.setLineWrap(true);
-        answerArea.setWrapStyleWord(true);
+        discussionListModel = new DefaultListModel<>();
+        discussionList = new JList<>(discussionListModel);
+        discussionList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         postQuestionButton = new JButton("Post Question");
         postQuestionButton.addActionListener(new ActionListener() {
@@ -35,7 +30,7 @@ public class Forum extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 String question = JOptionPane.showInputDialog("Enter your question:");
                 if (question != null && !question.trim().isEmpty()) {
-                    questionArea.append("Q: " + question + "\n");
+                    discussionListModel.addElement("Q: " + question);
                 }
             }
         });
@@ -44,12 +39,27 @@ public class Forum extends JPanel {
         postAnswerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                int selectedIndex = discussionList.getSelectedIndex();
+                if (selectedIndex == -1) {
+                    JOptionPane.showMessageDialog(null, "Please select a question to answer.");
+                    return;
+                }
+
                 String answer = JOptionPane.showInputDialog("Enter your answer:");
                 if (answer != null && !answer.trim().isEmpty()) {
-                    answerArea.append("A: " + answer + "\n");
+                    // Find the index of the last answer under the selected question
+                    int lastAnswerIndex = selectedIndex + 1;
+                    while (lastAnswerIndex < discussionListModel.size() &&
+                            !discussionListModel.getElementAt(lastAnswerIndex).startsWith("Q: ")) {
+                        lastAnswerIndex++;
+                    }
+
+                    // Insert the new answer under the selected question
+                    discussionListModel.add(lastAnswerIndex, "    A: " + answer);
                 }
             }
         });
+
     }
 
     private void addComponentsToPanel() {
@@ -58,8 +68,6 @@ public class Forum extends JPanel {
         buttonPanel.add(postAnswerButton);
 
         add(buttonPanel, BorderLayout.NORTH);
-        add(new JScrollPane(questionArea), BorderLayout.CENTER);
-        add(new JScrollPane(answerArea), BorderLayout.SOUTH);
+        add(new JScrollPane(discussionList), BorderLayout.CENTER);
     }
-
 }
